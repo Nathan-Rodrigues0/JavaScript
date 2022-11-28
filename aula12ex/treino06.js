167151daff8e47011990ffefce8099f3233026ed
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2022-11-28 16:30:02 -0300

    passando alguns teste.js

---
+++ aula12ex/treino06.js
@@ -0,0 +1,41 @@
+var agora = new Date()
+var mês = agora.getMonth()
+
+switch(mês) {
+    case 0:
+        console.log('Janeiro')
+        break
+    case 1:
+        console.log('Fevereiro')
+        break
+    case 2:
+        console.log('Março')
+        break
+    case 3:
+        console.log('Abril')
+        break
+    case 4:
+         console.log('Maio')
+         break
+     case 5:
+         console.log('Junho')
+         break
+     case 6:
+        console.log('Julho')
+        break
+    case 7:
+        console.log('Agosto')
+        break
+    case 8:
+        console.log('Setembro')
+        break
+     case 9:
+        console.log('Outubro')
+        break
+    case 10:
+        console.log('Novembro')
+        break
+    case 11:
+        console.log('Dezembro')
+        break
+}
---
+++ aula13/treino07.js
@@ -0,0 +1,5 @@
+var pizza = 1
+while (pizza <= 150) {
+    console.log(`Já foi ${pizza}`)
+    pizza++
+}
