f7f9e089147d0bafe3aeaacb095e183f6d14e978
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-01-11 16:51:21 -0300

    treinamdo html/css/js

---
+++ treinamento/multa.js
@@ -0,0 +1,11 @@
+function calcular() {
+    var num = window.document.getElementById('num1')
+    var res = window.document.getElementById('res')
+    var vel = Number(num.value)
+    res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}Km/h<strong></p>`
+    if (vel > 90) {
+        res.innerHTML += '<p>Você esta <strong>MULTADO</strong> por excesso de velocidade e ter ultrapassado o limite</p>'
+    } else {
+        res.innerHTML += '<p><strong>MUITO BEM</strong>. Dirige sempre com o cinto de segurança!</p>'
+    }
+}
