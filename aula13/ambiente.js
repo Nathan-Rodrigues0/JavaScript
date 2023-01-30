6b649fd64db325cb464b8731abb2830d74e428e5
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-01-30 16:24:06 -0300

    treinamdo repeticões em script

---
+++ aula13/ambiente.js
@@ -0,0 +1,31 @@
+var as = 1
+do {
+    console.log('eae meu chapa 1')
+    as++
+} while (as <= 40)
+
+
+
+
+
+
+
+
+
+
+/*var d = 1
+while (d <= 6) {
+    console.log('Tudo Bem!')
+    d++
+}*/
+
+
+
+/*aula do curso*/
+
+
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
