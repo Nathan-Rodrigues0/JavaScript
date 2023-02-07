d1fc2e0edc66ca159bbe95b9e9e997e881d43e3b
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-02-07 17:33:41 -0300

    tentando e prosseguindo

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,4 +1,14 @@
 function calcular() {
-
+    var i = window.document.getElementById('inicio')
+    var f = window.document.getElementById('fim')
+    var p = window.document.getElementById('passo')
+    var i = Number(i.value)
+    var f = Number(f.value)
+    var p = Number(p.value)
     var res = window.document.getElementById('res')
+    res.innerHTML = `<p>Contando</p><p>Passo ${i}</p>`
+    while(i <= f) {
+        console.log(i)
+        i++
+    }
 }
