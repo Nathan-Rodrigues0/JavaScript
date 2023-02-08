626902f1bf24fffd2eb3766d709c47e06ea48087
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-02-08 17:11:27 -0300

    .

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,9 +6,10 @@ function calcular() {
     var f = Number(f.value)
     var p = Number(p.value)
     var res = window.document.getElementById('res')
-    res.innerHTML = `<p>Contando</p><p>Passo ${i}</p>`
-    while(i <= f) {
-        console.log(i)
-        i++
-    }
+    res.innerHTML = `<p>Contando</p><p>Passo ${i}</p>` 
 }
+var i = 1
+ while(i <= f) {
+        console.log(i);
+        i++;
+    }
