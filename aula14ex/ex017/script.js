70ccc17f8b916813c2622cfb2a1f5d58040fd8f8
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-02-15 18:25:49 -0300

    criando o ex tabuadas

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,7 +1,12 @@
-var res = window.document.getElementById('res')
-res.innerHTML()
-
 function calcular() {
-
-
+    var
+    var num = Number(n1.value)
+    var res = window.document.getElementById('res')
+    if (n1 < 2) {
+       '<p>1 x 1 = 1</p><p>1 x 2 = 2</p>'
+    } else {
+        if (n1 < 3){
+            '<p>2 x 1 = 2</p>'
+        }
+    }
 }
