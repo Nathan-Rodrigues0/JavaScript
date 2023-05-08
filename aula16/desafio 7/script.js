3a1b8f843e7c6d1f935779983afa1f58f0a9b241
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-05-08 18:38:21 -0300

    desenvolvendo o exercicios em js

--- aula16/desafio 7/script.js
+++ aula16/desafio 7/script.js
@@ -1,7 +1,28 @@
 var num = document.getElementById('inum')
-var n = Number(num.value)
-
+var lista = document.querySelector('select#ilista')
+var res = document.getElementById('res')
+var valores = []
 
 function adicionar() {
-    
+    if(isNumero(num.value) && !inLista(num.value)) {
+        window.alert('Tudo ok!')
+} else {
+    window.alert('Valor inválido ou já encontrado na lista.')
+}
+}
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+    return true
+} else {
+    return false
+}
+}
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
 }
