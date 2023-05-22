aeb5b325646b235faa500c5231ce1c938ef2c79b
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-05-22 19:13:55 -0300

    terminando a parte 1

--- aula16/copias/de7/script.js
+++ aula16/copias/de7/script.js
@@ -1,1 +1,28 @@
-var num
+var num = document.getElementById('inum')
+var lis = document.getElementById('ilis')
+var res = document.getElementById('res')
+var valores = []
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value)) {
+        window.alert('Tudo ok!')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
