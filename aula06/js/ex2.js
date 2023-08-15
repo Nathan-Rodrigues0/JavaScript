6e236342097a6db7f0893f2b9136113952e98605
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-08-15 15:51:46 -0300

    atualizando

---
+++ aula06/js/ex2.js
@@ -0,0 +1,3 @@
+var nome = prompt('Qual é o seu nome?')
+
+alert('É um grande prazer em te conhecer ' + nome + ' !')
---
+++ aula06/js/ex3.js
@@ -0,0 +1,5 @@
+var num1 = Number(prompt('Digite um número'))
+var num2 = Number(prompt('Digite um número'))
+var soma = num1 + num2
+
+alert(`A soma entre ${num1} e ${num2} é igual a ${soma}`)
